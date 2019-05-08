5da453770fd9981317fdb386121e215a1a309510
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-08 17:24:18 +0800

    Getting notes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,1 +1,36 @@
-export default () => "Home";
+import React, { useState, useEffect } from "react";
+import { useUser } from "../userContext";
+import { API } from "aws-amplify";
+
+export default () => {
+  const [loading, setLoading] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const user = useUser();
+  useEffect(() => {
+    const getNotes = async () => {
+      const notes = await API.get("notes", "/notes");
+      setNotes(notes);
+      setLoading(false);
+    };
+    getNotes();
+  }, []);
+
+  return (
+    <>
+      <h1>Home</h1>
+      {user.isLoggedIn ? (
+        loading ? (
+          "Getting your notes..."
+        ) : (
+          <ul>
+            {notes.map(note => (
+              <li key={note.noteId}>👉🏻 {note.content}</li>
+            ))}
+          </ul>
+        )
+      ) : (
+        <h3>Log in to see your notes</h3>
+      )}
+    </>
+  );
+};
